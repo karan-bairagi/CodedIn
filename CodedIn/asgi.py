d7e1b1d956329ccc12bc5ffd1ca53e4a0754bf76
Karan Bairagi <karanbairagivaidik57@gmail.com>
2026-06-25 16:48:44 +0530

    fix railway dependencies

--- CodedIn/asgi.py
+++ CodedIn/asgi.py
@@ -1,16 +1,17 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CodedIn.settings')
-
-from channels.routing import ProtocolTypeRouter,URLRouter
+django.setup()
+django_asgi_app = get_asgi_application()
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.sessions import SessionMiddlewareStack
 from chat.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-    'http':get_asgi_application(),
-    'websocket':AuthMiddlewareStack(
-        SessionMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        )
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
     ),
 })
+
